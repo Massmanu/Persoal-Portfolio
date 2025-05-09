27ac5132fb0b5df4759b3dd68c818310c5562231
Massmanu <110564283+Massmanu@users.noreply.github.com>
2025-05-09 15:44:53 +1000

    Update index.js

--- src/componebts/Resume/index.js
+++ src/componebts/Resume/index.js
@@ -1,5 +1,5 @@
 const Resume = () => {
-    fetch('https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=10&playlistId=UUS2m4WDXYmfGGC9C4hayKug&key=AIzaSyAPEWO483cncsRKPXSfXD9Ye3kgs3XL3lI')
+    fetch('https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=10&playlistId="PLAYID"&key="KEY"')
         .then(res => res.json())
         .then(data => {
             console.log(data.items[1]);
